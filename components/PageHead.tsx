9213d272e3e1d1da6567d30107d96329f42b3329
heyhey1028 <shoheiogawa1028@gmail.com>
2023-05-05 23:56:34 +0900

    test: change meta tag order

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -29,8 +29,18 @@ export const PageHead: React.FC<
         content='width=device-width, initial-scale=1, shrink-to-fit=no'
       />
 
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fefffe" key="theme-color-light"/>
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#2d3439" key="theme-color-dark"/>
+      <meta
+        name='theme-color'
+        media='(prefers-color-scheme: light)'
+        content='#fefffe'
+        key='theme-color-light'
+      />
+      <meta
+        name='theme-color'
+        media='(prefers-color-scheme: dark)'
+        content='#2d3439'
+        key='theme-color-dark'
+      />
 
       <meta name='robots' content='index,follow' />
       <meta property='og:type' content='website' />
@@ -56,9 +66,9 @@ export const PageHead: React.FC<
 
       {socialImageUrl ? (
         <>
+          <meta property='og:image' content={socialImageUrl} />
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:image' content={socialImageUrl} />
-          <meta property='og:image' content={socialImageUrl} />
         </>
       ) : (
         <meta name='twitter:card' content='summary' />
